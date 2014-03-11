baba8f9665eeba38f5fe9ece749d6477ff7870e7
Marc Kohli <marckohli@gmail.com>
2014-03-11 16:47:16 -0400

    changes from awhile ago, including updates to some of the pipelines and stages

--- process.rb
+++ process.rb
@@ -4,17 +4,37 @@
 # Set the variables here in this script and then run it!
 require 'erb'
 
+if ARGV.length != 0 && File.exists?(ARGV[0])
+	template_file = ARGV[0]
+else
+	raise "Must supply a valid .erb file as an argument"
+end
 
-template_file = "basic-research-pipeline.erb"
+root_base = "roots/"
 
 b = binding
-name = "sswrist"
+name = "research"
+calling_aet = "sswrist"
 listening_port = 1085
 listening_ip = "134.68.158.28"
-destination_aet = "FRESHAIR"
-destination_ip = "134.68.161.109"
-destination_port = 4096
+
+# destination_aet = "FRESHAIR"
+# destination_ip = "134.68.161.109"
+# destination_port = 4096
+
+destination_aet = "CHSYNRSRCHSCP"
+destination_ip = "10.8.224.44"
+destination_port = 104
+
+
+###################
+# File Storage Service config
+###################
+
+file_service_root = File.join(root_base, name + "DicomAnon", "FileStorageService")
+file_service_web_port = nil
 
 template = ERB.new(File.read(template_file))
 
-puts template.result(b)
+output = template.result(b).gsub /^$\n/, ''
+puts output
