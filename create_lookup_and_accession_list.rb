8f3e91d4bb05ae4baac74093a08a43b1d3e11916
Marc Kohli (shadowdoc) <marckohli@gmail.com>
2014-06-10 11:09:11 -0400

    Updated create_lookup_and_accession_list.rb to be able to use random patient and accession numbers

--- create_lookup_and_accession_list.rb
+++ create_lookup_and_accession_list.rb
@@ -13,6 +13,8 @@ irb = 1307011922
 primary_investigator = "Dr. Dalsing - Vascular Surgery"
 contact = "Adam Gracon <agracon@iupui.edu>"
 salt = SecureRandom.base64
+random_patient_id = true
+random_accession_id = true
 
 unless ARGV.length ==  1 && File.exists?(ARGV[0])
 	raise "ERROR: Please include an existing CSV file as the first argument"
@@ -22,7 +24,15 @@ filename = ARGV[0].split(".")
 
 patients = []
 accessions = []
-new_accessions = []
+
+def create_random_list(l)
+	# This creates a random list of numbers of lenght l
+	# The numbers range from 100-10000
+
+	random_limit = 10000
+	a = (100...random_limit).sort_by{rand}
+	a[0..l]
+end
 
 original = CSV.read(ARGV[0], :headers => true)
 
@@ -39,29 +49,75 @@ end
 patients.uniq!
 accessions.uniq!
 
+
+# This is a safety thing based on the way that the random numbers are assigned.
+# This could obviosly be fixed if needed.
+
+if patients.length > 10000 || accessions.length > 10000
+	raise "ERROR: Can't work with patient or accession lists larger than 10000"
+end
+
 # Create the new_patients hash
 # This stores our old MRNs (key) and the Study MRNs (value)
 
 new_patients = {}
 
+if random_patient_id
+	
+	# This creates a random list of new IDs
+	new_id_list = create_random_list(patients.length)
+
+else
+
+	# This creates a sequential list of new IDs
+	new_id_list = (100...patients.length)
+
+end
+
 patients.each_with_index do |p,i|
 
 	# For patient with MRN 93747 and IRB 9876543210 that is the first in the list
-	# this would produce the following line
+	# this would produce the following line if we're using non-random IDs
 	# ptid/93747=9876543210-101
 
-	new_patients[p] = "#{irb}-#{100 + i}"
+	# If we're using random IDs then 101 would be replaced with a random number 
+	# between 100 and 10000 + length of the patient list
+
+	puts "Old ID: #{p}, New ID: #{new_id_list[i]}" if DEBUG
+
+	new_patients[p] = "#{irb}-#{new_id_list[i].to_s.rjust(5,"0")}"
 
 end
+
+# Here we check the results of our work to make sure that we don't have any duplicates
+raise "ERROR: Patient Number Generation failed all entries are not unique" if new_patients.values.uniq != new_patients.values
 
 # Create the new_accessions hash
 # This stores our old accession (key) and the Study Accession (value)
 
 new_accessions = {}
 
+if random_accession_id
+	
+	# This creates a random list of new IDs
+	new_id_list = create_random_list(accessions.length)
+
+else
+
+	# This creates a sequential list of new IDs
+	new_id_list = (100...accessions.length)
+
+end
+
 accessions.each_with_index do |a,i|
 
-	new_accessions[a] = "#{irb}-#{10000 + i}" 
+	# This creates a hash with the new accession numbers
+	# the old accession number is stored as the key, and the new as the value
+
+	new_accessions[a] = "#{irb}-#{new_id_list[i].to_s.rjust(5,"0")}" 
+
+
+	raise "ERROR: Accession Number too long for DICOM standard" if new_accessions[a].length > 16
 
 end
 
@@ -95,7 +151,7 @@ File.open("#{irb}-lookup.properties", 'w') do |f|
 		end
 
 		# The lookup original for accession numbers just identifies the IRB that's associated with a particular study
-		# Synapse uses the IRB number previx as a trigger to sort
+		# Synapse uses the IRB number prefix as a trigger to sort into worklists
 
 		new_accessions.each do |old_a, new_a|
 
